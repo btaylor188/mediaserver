a2ffdedefa238333a7306e474cc3bfe96fdec478
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-07 16:39:17 -0600

    Update all_apps_slient_deb.sh

--- all_apps_slient_deb.sh
+++ all_apps_slient_deb.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+read -p "Did you run as sudo? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
 
 #prompt for config info
 echo "What is the domain name?"
