eef9bd956162b1e6a45ec28545247a1d2ae1f6a8
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-07 16:16:32 -0600

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,32 @@
+#! /bin/bash 
+echo "What is the domain name?"
+read DOMAINNAME
+echo "Enter path for Docker data.  ie. /mnt/docker"
+read DOCKERPATH
+# Install Speedtest
+echo "Installing Speedtest"
+
+--name=speedtest \
+-p 127.0.0.1:8223:8223 \
+-e PUID=1000 \
+-e PGID=1000 \
+-e TITLE=$DOMAINNAME \
+-e WEBPORT=8223 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-l "traefik.enable"="true" \
+-l "traefik.frontend.auth.forward.address"="http://oauth:4181" \
+-l "traefik.frontend.headers.SSLHost"="$DOMAINNAME" \
+-l "traefik.frontend.headers.SSLRedirect"="true" \
+-l "traefik.frontend.headers.STSIncludeSubdomains"="true" \
+-l "traefik.frontend.headers.STSPreload"="true" \
+-l "traefik.frontend.headers.STSSeconds"="315360000" \
+-l "traefik.frontend.headers.browserXSSFilter"="true" \
+-l "traefik.frontend.headers.contentTypeNosniff"="true" \
+-l "traefik.frontend.headers.customResponseHeaders"="X-Robots-Tag:noindex,nofollow,nosnippet,noarchive,notranslate,noimageindex" \
+-l "traefik.frontend.headers.forceSTSHeader"="true" \
+-l "traefik.frontend.rule"="Host:speedtest.$DOMAINNAME,speedtest.$DOMAINNAME" \
+-l "traefik.port"="8223" \
+--restart=always \
+adolfintel/speedtest
+docker network create -d bridge --subnet=172.18.0.0/24 internal
+docker network connect internal speedtest
