f67a95afd83368d7d436764cfbaa38a1a7e436b3
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-07 17:20:39 -0600

    Update all_apps_slient_centos.sh

--- all_apps_slient_centos.sh
+++ all_apps_slient_centos.sh
@@ -513,3 +513,39 @@ docker run -d \
 thomseddon/traefik-forward-auth > install.log 2>&1
 
 docker network connect internal oauth > install.log 2>&1
+
+#Install PLex
+docker run \
+-d \
+--name plex \
+--device /dev/dri:/dev/dri \
+-p 32400:32400/tcp \
+-p 3005:3005/tcp \
+-p 8324:8324/tcp \
+-p 32469:32469/tcp \
+-p 1900:1900/udp \
+-p 32410:32410/udp \
+-p 32412:32412/udp \
+-p 32413:32413/udp \
+-p 32414:32414/udp \
+-e TZ=America/Denver \
+-l "traefik.enable"="true" \
+-l "traefik.frontend.auth.forward.address"="http://oauth:4181" \
+-l "traefik.frontend.headers.SSLHost"="$DOMAINNAME" \
+-l "traefik.frontend.headers.SSLRedirect"="true" \
+-l "traefik.frontend.headers.STSIncludeSubdomains"="true" \
+-l "traefik.frontend.headers.STSPreload"="true" \
+-l "traefik.frontend.headers.STSSeconds"="315360000" \
+-l "traefik.frontend.headers.browserXSSFilter"="true" \
+-l "traefik.frontend.headers.contentTypeNosniff"="true" \
+-l "traefik.frontend.headers.customResponseHeaders"="X-Robots-Tag:noindex,nofollow,nosnippet,noarchive,notranslate,noimageindex" \
+-l "traefik.frontend.headers.forceSTSHeader"="true" \
+-l "traefik.frontend.rule"="Host:portainer.$DOMAINNAME" \
+-l "traefik.port"="32400" \
+-v $DOCKERPATH/plex/database:/config \
+-v /dev/shm:/transcode \
+-v $DOCKERPATH/plex:/mnt/unionfs/Media \
+plexinc/pms-docker > install.log 2>&1
+
+docker network connect internal plex > install.log 2>&1
+
