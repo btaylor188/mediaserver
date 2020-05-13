8ea00a46e6f6514f00e4d702bd58d0e78e82c75b
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-12 23:01:27 -0600

    Update and rename handbrake.sh to handbrake_nvenc.sh

--- handbrake_nvenc.sh
+++ handbrake_nvenc.sh
@@ -1,36 +1,23 @@
-#! /bin/bash 
+#! /bin/bash
+
 echo "What is the domain name?"
 read DOMAINNAME
 echo "Enter path for Docker data.  ie. /mnt/docker"
 read DOCKERPATH
-#Install HandBrake
-echo "Installing HandBrake"
- docker run -d \
+echo "Location of watch folder?"
+read WATCH
+echo "Location of output folder?"
+read OUTPUT
+
+docker kill handbrake
+docker rm handbrake
+
+docker run -d -t \
 --name=handbrake \
--p 127.0.0.1:5800:5800 \
---device /dev/dri:/dev/dri \
--e GROUP_ID=1000 \
--e USER_ID=1000 \
--e AUTOMATED_CONVERSION_PRESET=myh265 \
--e AUTOMATED_CONVERSION_FORMAT=mkv \
--e XDG_CONFIG_HOME=/config/xdg \
--e LANG=en_US.UTF-8 \
--e S6_BEHAVIOUR_IF_STAGE2_FAILS=3 \
--e S6_SERVICE_DEPS=1 \
--e APP_NAME=HandBrake \
--e APP_USER=app \
--e XDG_DATA_HOME=/config/xdg/data \
--e XDG_CONFIG_HOME=/config/xdg/config \
--e XDG_CACHE_HOME=/config/xdg/cache \
--e XDG_RUNTIME_DIR=/tmp/run/user/app \
--e DISPLAY=:0 \
--e DISPLAY_WIDTH=1280 \
--e DISPLAY_HEIGHT=768 \
--v /etc/localtime:/etc/localtime \
--v $DOCKERPATH/handbrake/config:/config \
--v /mnt:/mnt \
--v /mnt/docker/handbrake/watch/my265:/watch \
--v /mnt/docker/handbrake/complete:/output  \
+-p 5800:5800 \
+-v $DOCKERPATH:/config \
+-v $WATCH:/watch:rw \
+-v $OUTPUT:/output:rw \
 -l "traefik.enable"="true" \
 -l "traefik.frontend.auth.forward.address"="http://oauth:4181" \
 -l "traefik.frontend.headers.SSLHost"="$DOMAINNAME" \
@@ -44,7 +31,8 @@ echo "Installing HandBrake"
 -l "traefik.frontend.headers.forceSTSHeader"="true" \
 -l "traefik.frontend.rule"="Host:handbrake.$DOMAINNAME,handbrake.$DOMAINNAME" \
 -l "traefik.port"="5800" \
---restart=always \
-jlesage/handbrake
+--gpus all \
+zocker160/handbrake-nvenc:latest
+
 docker network create -d bridge --subnet=172.18.0.0/24 internal
 docker network connect internal handbrake
