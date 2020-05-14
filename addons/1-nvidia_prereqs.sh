395a072e7473c86e07bdd84db4366f394987c57c
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-13 19:22:14 -0600

    Update 1-nvidia_prereqs.sh

--- addons/1-nvidia_prereqs.sh
+++ addons/1-nvidia_prereqs.sh
@@ -2,6 +2,11 @@
 echo "Configuring Driver Blacklist"
 sudo echo 'blacklist nouveau' >> /etc/modprobe.d/blacklist-nouveau.conf
 sudo echo 'options nouveau modeset=0' >> /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo 'blacklist vga16fb' >> /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo 'blacklist nouveau' >> /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo 'blacklist rivafb' >> /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo 'blacklist nvidiafb' >> /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo 'blacklist rivatv' >> /etc/modprobe.d/blacklist-nouveau.conf
 sudo update-initramfs -u 
  
 echo "#################################################"
