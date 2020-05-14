04f4ee30db3f1a937f4650ba8a60baec28f97677
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-13 19:12:22 -0600

    Update 2-nvidia_drivers.sh

--- addons/2-nvidia_drivers.sh
+++ addons/2-nvidia_drivers.sh
@@ -7,7 +7,8 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 sudo apt-get install gcc binutils make linux-source pve-headers nvidia-docker2 nvidia-container-runtime -y
-#sudo apt install linux-headers-$(uname -r)
+sudo apt install linux-headers-$(uname -r)
+sudo apt install pve-headers
 sudo yum install kernel-devel dkms gcc make perl bin utils linux-source nvidia-docker2 nvidia-container-runtime -y
 
 
