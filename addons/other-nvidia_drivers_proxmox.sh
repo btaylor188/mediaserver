abafee170c943f18c16897b02a1cb91377a74f97
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-13 19:53:00 -0600

    Update other-nvidia_drivers_proxmox.sh

--- addons/other-nvidia_drivers_proxmox.sh
+++ addons/other-nvidia_drivers_proxmox.sh
@@ -22,13 +22,9 @@ sudo apt-get install -y libglvnd-dev
 ### Download and install drivers ###
 sudo rm -rf /opt/nvidia
 sudo mkdir /opt/nvidia && cd /opt/nvidia
-pause
 sudo wget https://international.download.nvidia.com/XFree86/Linux-x86_64/440.82/NVIDIA-Linux-x86_64-440.82.run
-pause
 sudo wget http://download.proxmox.com/debian/pve/dists/buster/pvetest/binary-amd64/pve-headers-$(uname -r)_$(uname -r | perl -pe '($_)=/([0-9]+([.-][0-9]+)+)/')_amd64.deb
-pause
 sudo dpkg -i pve-headers-$(uname -r)_$(uname -r | perl -pe '($_)=/([0-9]+([.-][0-9]+)+)/')_amd64.deb
-pause
 sudo chmod +x /opt/nvidia/NVIDIA-Linux-x86_64-440.82.run
 sudo ./NVIDIA-Linux-x86_64-440.82.run --kernel-source-path /usr/src/linux-headers-$(uname -r)/ 
 
