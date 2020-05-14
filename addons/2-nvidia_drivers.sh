b7ac525feb0b7f3d061706618ca5214f3f247453
brandon <brandon@centos7.int.interwebnet.net>
2020-05-13 18:09:42 -0600

    added centos line

--- addons/2-nvidia_drivers.sh
+++ addons/2-nvidia_drivers.sh
@@ -7,7 +7,7 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 sudo apt-get install gcc binutils make linux-source pve-headers linux-headers-$(uname -r) nvidia-docker2 nvidia-container-runtime
-
+sudo yum install kernel-devel gcc make perl bin utils linux-source
 
 
 ### Download and install drivers ###
