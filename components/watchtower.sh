e00ce2b1f05d18d9f6b416fd5ea06cb1c765d13e
btaylor188 <brandon@deepthought.int.interwebnet.net>
2021-01-17 18:21:35 -0700

    made homeassistant execuitable

--- components/watchtower.sh
+++ components/watchtower.sh
@@ -6,6 +6,6 @@ docker run -d \
 --name watchtower \
 -v /var/run/docker.sock:/var/run/docker.sock \
 --restart=always \
---schedule 0 4 * * 0 \
- containrrr/watchtower > watchtower.log 2>&1
+-e WATCHTOWER_POLL_INTERVAL=86400 \
+containrrr/watchtower
 
